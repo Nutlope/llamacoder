79816a15970a0de3cfd73f649c51d62625a3d9b7
Ryan Toronto <ryanto@gmail.com>
2024-12-30 12:26:37 -0500

    github repo

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -57,8 +57,18 @@ export default function Home() {
       </div>
 
       <div className="isolate flex h-full flex-col">
-        <header className="shrink-0 py-5">
+        <header className="relative mx-auto flex w-full shrink-0 items-center justify-center py-5">
           <Image src={logo} alt="" className="mx-auto h-6 w-auto" priority />
+          <div className="absolute right-3">
+            <a
+              href="https://github.com/nutlope/llamacoder"
+              target="_blank"
+              className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+            >
+              <GithubIcon className="h-4 w-4" />
+              <span>GitHub Repo</span>
+            </a>
+          </div>
         </header>
 
         <div className="mt-16 flex grow flex-col items-center px-4 lg:mt-24">
