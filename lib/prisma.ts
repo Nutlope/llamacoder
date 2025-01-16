37ad88fe1c1568496d635b1491859fa3da519a98
Sam Selikoff <sam.selikoff@gmail.com>
2025-01-16 15:13:10 -0500

    wip

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(neon);
+const client = globalThis.prisma || new PrismaClient({ adapter });
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export default client;
