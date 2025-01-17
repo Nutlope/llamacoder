8f030385916f98c94ea06871694984bfc71a498d
Sam Selikoff <sam.selikoff@gmail.com>
2025-01-17 14:45:56 -0500

    Comment out custom webpack

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@codesandbox/sdk"],
-  webpack: (config, options) => {
-    if (options.nextRuntime === "edge") {
-      if (!config.resolve.conditionNames) {
-        config.resolve.conditionNames = ["require", "node"];
-      }
-      if (!config.resolve.conditionNames.includes("worker")) {
-        config.resolve.conditionNames.push("worker");
-      }
-    }
-    return config;
-  },
+  // webpack: (config, options) => {
+  //   if (options.nextRuntime === "edge") {
+  //     if (!config.resolve.conditionNames) {
+  //       config.resolve.conditionNames = ["require", "node"];
+  //     }
+  //     if (!config.resolve.conditionNames.includes("worker")) {
+  //       config.resolve.conditionNames.push("worker");
+  //     }
+  //   }
+  //   return config;
+  // },
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
