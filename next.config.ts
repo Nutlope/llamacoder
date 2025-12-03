9bf2d820c22a402d866ff881ea1eb7bafb683d80
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-12-03 21:13:02 +0100

    new next!

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   webpack: (config, options) => {
     if (options.nextRuntime === "edge") {
       if (!config.resolve.conditionNames) {
