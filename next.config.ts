54f15aaf66375fe540d958c2aae36612e61e5fa3
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2026-07-02 17:01:15 +0200

    cloudflared support

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["*.trycloudflare.com"],
   turbopack: {},
   webpack: (config, options) => {
     if (options.nextRuntime === "edge") {
